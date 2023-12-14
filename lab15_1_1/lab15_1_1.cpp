998c9b4a896a908dee82b9663283393e5d710059
Ваня <@IVAN>
2023-12-14 18:33:17 +0200

    Добавьте файлы проекта.

---
+++ lab15_1_1/lab15_1_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main() {
+    double f, s;
+    printf("Input the first number\n");
+    scanf_s("%lf", &f);
+    printf("Input the second number\n");
+    scanf_s("%lf", &s);
+    if (f > s) {
+        printf("%f", f);
+    }
+    else {
+        printf("%f, %f", f, s);
+    }
+
+    return 0;
+}
